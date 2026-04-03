8a03ed89cb7cfeb64c2cc60c709a95b808bed7e8
= <ngstafford1@gmail.com>
2026-04-03 06:43:17 -0400

    fixed import

--- src/rawr_analytics/sources/nba_api/_normalize.py
+++ src/rawr_analytics/sources/nba_api/_normalize.py
@@ -4,7 +4,7 @@ from rawr_analytics.shared.game import NormalizedGamePlayerRecord, NormalizedGam
 from rawr_analytics.shared.player import PlayerSummary
 from rawr_analytics.shared.season import Season
 from rawr_analytics.shared.team import Team
-from rawr_analytics.sources.nba_api.models import (
+from rawr_analytics.sources.nba_api._models import (
     SourceBoxScore,
     SourceBoxScorePlayer,
     SourceBoxScoreTeam,
